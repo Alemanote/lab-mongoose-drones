39da31f90dfd5119416cef6ccde1b56101444b00
Alberto Fernandez <fernandez.gandarias@gmail.com>
2017-06-24 14:46:01 +0200

    Hasta Iteracion 3

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -3,10 +3,18 @@ const express = require('express');
 // require the Drone model here
 
 const router = express.Router();
-
+const Drone = require('../models/drone');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+
+  Drone.find({}, (err, docs) => {
+    if (err) { return next(err) }
+    
+    res.render('drones/index', {
+      drones: docs
+      });
+});
 });
 
 
@@ -17,5 +25,6 @@ router.get('/drones/new', (req, res, next) => {
 router.post('/drones', (req, res, next) => {
   // Iteration #3
 });
+
 
 module.exports = router;
